0eb0f7c2a9cef645ded119534b5287e906fb000b
Aditya Patidar <37375907+adpatidar@users.noreply.github.com>
2023-09-23 21:34:30 +0530

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Gajanan Sir Ji JIIIIIIIII has successfuly built a spring boot application using Maven");
+        model.addAttribute("title", "Successfuly deployed a spring boot application");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
